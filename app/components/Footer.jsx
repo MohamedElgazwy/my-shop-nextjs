4e85dc421cd6349682783efed1538281d6ef7366
Mohamed Aljazwi <almyhwbmhmd26@gmail.com>
2025-11-14 23:15:13 +0200

    Update footer text with developer's name

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="text-center py-2">
       <h2>
-        Made by 💖<span className="font-mono">جازوى مصرى</span>
+        Made by 💖<span className="font-mono">Mohamed Aljazwi</span>
       </h2>
     </div>
   );
